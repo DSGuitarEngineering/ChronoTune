27786fd4310991271b5e76d6cd38239fbb48f317
Daniel Shields <daniel@dsguitarengineering.com>
2018-01-28 12:13:36 -0500

    added comments to code

--- Firmware/ChronoTune/ChronoTune.ino
+++ Firmware/ChronoTune/ChronoTune.ino
@@ -19,11 +19,13 @@ Modified:       January 27, 2018
 Verified:       January 27, 2018
 Target uC:      ATSAMD21G18
 
------------------------------------------***DESCRIPTION***
+-----------------------------------------***DESCRIPTION***------------------------------------------------
 This project is based on the Chronograph, but also includes circuitry for a chromatic tuner.  The tuner
 and clock will share a 4-character alphanumeric display.  The basic premise is that the pedal will
 normally operate as a Chronogrpah, but instant access to a tuner will be available at any time.  Another
 main goal is to allow user updateable drag-and-drop firmware via USB and UF2 bootloader.
+
+Project ChronoTune was developed in collaboration with the Inner Circle of the 60 Cycle Hum podcast.
 
 --------------------------------------------------------------------------------------------------------*/
 //include the necessary libraries
@@ -31,18 +33,19 @@ main goal is to allow user updateable drag-and-drop firmware via USB and UF2 boo
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
 
-Adafruit_AlphaNum4 alpha4 = Adafruit_AlphaNum4();
+Adafruit_AlphaNum4 alpha4 = Adafruit_AlphaNum4();  //initialize the display
 
-char displaybuffer[4] = {' ', ' ', ' ', ' '};
+char displaybuffer[4] = {' ', ' ', ' ', ' '};  //buffer for marquee messages
 
 void setup() {
-  Serial.begin(9600);
-  
+  Serial.begin(9600);  //open the serial port (for debug only)
+
   alpha4.begin(0x70);  // pass in the address
 
-  alpha4.clear();
-  alpha4.writeDisplay();
+  alpha4.clear();         //clear the display
+  alpha4.writeDisplay();  //use this command to update the display
 
+  //display startup message
   marquee("Project ChronoTune");
   delay(500);
   marquee("by DS Engineering");
@@ -50,32 +53,34 @@ void setup() {
 
 
 void loop() {
-   
+
 }
 
 //This routine displays marquee style messages--------------------------------------------------------------
+//  must pass a string; returns nothing
 void marquee(String s)
 {
-  s.concat("    ");
-  for (int i=0; i < s.length(); i++){
-    
+  s.concat("    ");  //concatenate 4 blank spaces to  the beginning of the message to make the entire
+                     //message scroll across the display
+
+  for (int i=0; i < s.length(); i++){   //run loop i times, where i is the number of characters in the string
+
     // scroll down display
     displaybuffer[0] = displaybuffer[1];
     displaybuffer[1] = displaybuffer[2];
     displaybuffer[2] = displaybuffer[3];
     displaybuffer[3] = s.charAt(i);
- 
+
     // set every digit to the buffer
     alpha4.writeDigitAscii(0, displaybuffer[0]);
     alpha4.writeDigitAscii(1, displaybuffer[1]);
     alpha4.writeDigitAscii(2, displaybuffer[2]);
     alpha4.writeDigitAscii(3, displaybuffer[3]);
- 
-    // write it out!
+
+    // write it out
     alpha4.writeDisplay();
-    delay(200);
+    delay(200);           //delay between each character transition
   }
   alpha4.clear();
   alpha4.writeDisplay();
 }
-
